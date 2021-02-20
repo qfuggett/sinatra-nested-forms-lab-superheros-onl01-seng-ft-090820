fdafad742f96e9d7836ca49b7a877f5826ce9520
QueenTesa Fuggett <qfuggett@umich.edu>
2021-02-20 00:34:16 -0500

    commit tree 7642efd1dd8e364592dfc0706061ea3025c1b0fd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,25 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    erb :super_hero
+    erb :'super_hero'
   end
   
   post '/teams' do
-    binding.pry
-    @params
-    erb:team
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+
+    @hero_1_name = params[:team][:members][0][:name]
+    @hero_1_power = params[:team][:members][0][:power]
+    @hero_1_bio = params[:team][:members][0][:bio]
+
+    @hero_2_name = params[:team][:members][1][:name]
+    @hero_2_power = params[:team][:members][1][:power]
+    @hero_2_bio = params[:team][:members][1][:bio]
+
+    @hero_3_name = params[:team][:members][2][:name]
+    @hero_3_power = params[:team][:members][2][:power]
+    @hero_3_bio = params[:team][:members][2][:bio]
+    erb :'team'
   end
+
 end
